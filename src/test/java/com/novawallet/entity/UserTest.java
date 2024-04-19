a871ef39bf2781f16d6c5efc1614af0f661dbf8e
sara-rioseco <sara.rioseco@gmail.com>
2024-04-18 21:05:02 -0400

    add a few more tests, up to 100% coverage on entity folder

--- src/test/java/com/novawallet/entity/UserTest.java
+++ src/test/java/com/novawallet/entity/UserTest.java
@@ -50,7 +50,7 @@ class UserTest {
         Transaction transaction = mock(Transaction.class);
         user.addTransaction(transaction);
         assertEquals(1, user.getTransactions().size());
-        assertEquals(transaction, user.getTransactions().get(0));
+        assertEquals(transaction, user.getTransactions().getFirst());
     }
 
     @Test
@@ -59,7 +59,7 @@ class UserTest {
         Contact contact = mock(Contact.class);
         user.addContact(contact);
         assertEquals(1, user.getContacts().size());
-        assertEquals(contact, user.getContacts().get(0));
+        assertEquals(contact, user.getContacts().getFirst());
     }
 
     @Test
@@ -67,5 +67,90 @@ class UserTest {
         User user = new User(1, "John", "Doe", "john.doe@example.com", "password");
         String fullName = user.getFullName();
         assertEquals("John Doe", fullName);
+    }
+
+    @Test
+    public void testGetLastname(){
+        User user = new User(1, "John", "Doe", "john.doe@example.com", "password");
+        String lastname = user.getLastname();
+        assertEquals("Doe", lastname);
+    }
+
+    @Test
+    public void testSetLastname() {
+        User user = new User(1, "John", "Doe", "john.doe@example.com", "password");
+        user.setLastname("Smith");
+        assertEquals("Smith", user.getLastname());
+    }
+
+    @Test
+    public void testGetEmail(){
+        User user = new User(1, "John", "Doe", "john.doe@example.com", "password");
+        String email= user.getEmail();
+        assertEquals("john.doe@example.com", email);
+    }
+
+    @Test
+    public void testSetEmail() {
+        User user = new User(1, "John", "Doe", "john.doe@example.com", "password");
+        user.setEmail("newemail@example.com");
+        assertEquals("newemail@example.com", user.getEmail());
+    }
+
+    @Test
+    public void testGetPassword(){
+        User user = new User(1, "John", "Doe", "john.doe@example.com", "password");
+        String password = user.getPassword();
+        assertEquals("password", password);
+    }
+
+    @Test
+    public void testSetPassword() {
+        User user = new User(1, "John", "Doe", "john.doe@example.com", "password");
+        user.setPassword("SuperSecurePassword");
+        assertEquals("SuperSecurePassword", user.getPassword());
+    }
+
+    @Test
+    public void testGetRole(){
+        User user = new User(1, "John", "Doe", "john.doe@example.com", "password");
+        String role = user.getRole();
+        assertEquals("user", role);
+    }
+
+    @Test
+    public void testSetRole() {
+        User user = new User(1, "John", "Doe", "john.doe@example.com", "password");
+        user.setRole("admin");
+        assertEquals("admin", user.getRole());
+    }
+
+    @Test
+    public void testGetCurrency(){
+        User user = new User(1, "John", "Doe", "john.doe@example.com", "password");
+        String currency = user.getCurrency();
+        assertEquals("CLP", currency);
+    }
+
+    @Test
+    public void testSetTransactions() {
+        User user = new User(1, "John", "Doe", "john.doe@example.com", "password");
+        ArrayList<Transaction> transactions = new ArrayList<>();
+        Transaction transaction = mock(Transaction.class);
+        transactions.add(transaction);
+        user.setTransactions(transactions);
+        assertEquals(1, user.getTransactions().size());
+        assertEquals(transaction, user.getTransactions().getFirst());
+    }
+
+    @Test
+    public void testSetContacts() {
+        User user = new User(1, "John", "Doe", "john.doe@example.com", "password");
+        ArrayList<Contact> contacts = new ArrayList<>();
+        Contact contact = mock(Contact.class);
+        contacts.add(contact);
+        user.setContacts(contacts);
+        assertEquals(1, user.getContacts().size());
+        assertEquals(contact, user.getContacts().getFirst());
     }
 }
